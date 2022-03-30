ff62f3766594d9092a65c28e47f6e359831501fa
Mukul Debnath <debnath.mukul@gmail.com>
2022-03-30 07:09:18 +0530

    Fixed Login Layout, added pre-commit hook to build

---
+++ ui/static/ui/jquery.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('#id_username').addClass("w-50");
+    $('#div_id_password').addClass("mt-3");
+    $('#id_password').addClass("w-50");
+    $('button').removeClass("btn-outline-info").addClass("btn-dark mt-2");
+});
