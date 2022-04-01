47e8f9d09c827d3eb40d18e3ba37afa5c5a31291
Mukul Debnath <debnath.mukul@gmail.com>
2022-04-02 01:17:43 +0530

    Delete API task model

--- api/models.py
+++ api/models.py
@@ -13,26 +13,3 @@ def generate_unique_session_id():
 
 def upload_to(instance, filename):
     return f'{instance.user}/{instance.session_id}/{filename}'
-
-class Task(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updates_at = models.DateTimeField(auto_now=True)
-    session_id = models.CharField(max_length=settings.SESSION_ID_MAX_LENGTH, default=generate_unique_session_id, unique=True)
-    user = models.EmailField(max_length=254, default='trdjadmin@hobbycodes.com')
-    id_file_name = models.FileField(upload_to=upload_to)
-    steps_file_name = models.FileField(upload_to=upload_to)
-    attachment_file_name = models.FileField(upload_to=upload_to, blank=True, null=True)
-    project_id = models.IntegerField(default=0)
-    project_name = models.CharField(max_length=254, default='', blank=True, null=True)
-    suite_id = models.IntegerField(default=0)
-    suite_name = models.CharField(max_length=254, default='', blank=True, null=True)
-    section_id = models.IntegerField(default=0)
-    section_name = models.CharField(max_length=254, default='', blank=True, null=True)
-    retry_import = models.BooleanField(default=False)
-    total_cases = models.IntegerField(default=0)
-    imported_cases = models.IntegerField(default=0)
-    elapsed_time = models.DurationField(default=0)
-    status = models.CharField(max_length=20, default='', blank=True, null=True)
-
-    def __str__(self):
-        return "Session Id: " + self.session_id + ", User: " + self.user
