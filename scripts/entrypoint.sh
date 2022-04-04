a3fbd360d338d9569a2e070c513bd925057ba309
Mukul Debnath <debnath.mukul@gmail.com>
2022-04-04 21:25:32 +0530

    Remove Daphne Debug

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-daphne -v 3 -b 0.0.0.0 -p 8000 testrail_import_app.asgi:application
+daphne -b 0.0.0.0 -p 8000 testrail_import_app.asgi:application
