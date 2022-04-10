ddb80767e80b62873634b791b15ce5fdf3dee288
Mukul Debnath <debnath.mukul@gmail.com>
2022-04-10 21:50:59 +0530

    events test

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
 celery -A testrail_import_app worker -l info --detach
-daphne -b 0.0.0.0 -p 8000 testrail_import_app.asgi:application
+daphne -b 0.0.0.0 -p 8000 -v 3 --access-log /vol/web/daphne.log testrail_import_app.asgi:application
